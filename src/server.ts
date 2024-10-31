922c65e3613780dc45e360e170903e3154506aed
YiEnTsai-91 <yeenetsai@gmail.com>
2024-10-30 23:22:23 -0700

    Update

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,7 @@
 // Check the README.md file for instructions to the exercise
+import http from 'http'
+import fs from 'fs'
+import path from 'path'
+import { Http2ServerRequest, Http2ServerResponse } from 'http2'
+
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => { })
